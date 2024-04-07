91efb72ef2ab048119c0b531387bb11f5aaa3289
Heshi <159197956+Its-Heshi@users.noreply.github.com>
2024-04-07 16:45:09 +0530

    nooby update and refactoring

--- client/client.lua
+++ client/client.lua
@@ -1,89 +1,66 @@
-local enabled = false
+-- Global variables
+local sundayDrivingEnabled = false
+local speedMultiplier = (Config.speedunits == 'km/h') and 3.6 or 2.236936
 
-if Config.speedunits == 'km/h' then
-    speedMultiplier = 3.6
-else    
-    speedMultiplier = 2.236936
+-- Function to calculate speed limit based on street name
+local function CalculateSpeedLimit(streetName)
+    local speedLimit = Config.defaultspeedlimit
+    for i = 1, #Config.streets do
+        if streetName == Config.streets[i].name then
+            speedLimit = Config.streets[i].speed
+            break
+        end
+    end
+    return speedLimit
 end
 
-local function EngageSpeedLimit()
-    CreateThread(function()
-        while enabled do
-
-            if cache.seat then
-
-                local veh = GetVehiclePedIsIn(PlayerPedId())
-                local pos = GetEntityCoords(PlayerPedId())
-                local street = GetStreetNameAtCoord(pos.x, pos.y, pos.z)
-                local streetName = GetStreetNameFromHashKey(street)
-
-                for i = 1, #Config.streets do
-                    if streetName == Config.streets[i].name then
-                        speedLimit = Config.streets[i].speed
-                        break
-                    else
-                        speedLimit = Config.defaultspeedlimit
-                    end
-                end
-
-                if GetEntitySpeed(cache.vehicle) * speedMultiplier <= speedLimit then
-                    SetVehicleMaxSpeed(cache.vehicle, speedLimit / speedMultiplier)
-                end
-
-            end
-            Wait(250)
-        end
-    end)
-end
-
-local function EngageRpmLimit()
-    while enabled do
-
-        local veh = GetVehiclePedIsIn(PlayerPedId())
-
-        if cache.seat then
-
-            local maxRpm = 0.2 + (GetEntitySpeed(veh) / (Config.rpmlimiter * GetVehicleCurrentGear(veh)))
-
-            if GetVehicleCurrentRpm(cache.vehicle) > maxRpm then
-                SetVehicleCurrentRpm(cache.vehicle, maxRpm)
-            end
-
-        end
-        Wait(10)
+-- Function to apply speed limit to vehicle
+local function ApplySpeedLimit(veh, speedLimit)
+    local currentSpeed = GetEntitySpeed(veh) * speedMultiplier
+    if currentSpeed <= speedLimit then
+        SetVehicleMaxSpeed(veh, speedLimit / speedMultiplier)
     end
 end
 
-local keybind = lib.addKeybind({
-    name = 'sundaydriving',
-    description = 'Sunday Driving',
-    defaultKey = Config.keybind,
-    onPressed = function(self)
-        if enabled then
-            if Config.notify then
-                lib.notify({description = Config.notifydisengaged, type = 'inform'})
-            end
-            enabled = false
-            SetVehicleMaxSpeed(cache.vehicle, 300 / speedMultiplier)
-        else
-            enabled = true
+-- Function to engage/disengage Sunday driving limits
+local function EngageDrivingLimits()
+    sundayDrivingEnabled = not sundayDrivingEnabled
+    local notifyType = sundayDrivingEnabled and 'success' or 'inform'
+    local notifyMessage = sundayDrivingEnabled and Config.notifyengaged or Config.notifydisengaged
+    lib.notify({description = notifyMessage, type = notifyType})
+    while sundayDrivingEnabled do
+        if cache.seat then
+            local veh = cache.vehicle
+            local pos = GetEntityCoords(cache.ped)
+            local street = GetStreetNameAtCoord(pos.x, pos.y, pos.z)
+            local streetName = GetStreetNameFromHashKey(street)
+            -- Apply speed limit
             if Config.speedlimiter then
-                EngageSpeedLimit()
+                local speedLimit = CalculateSpeedLimit(streetName)
+                ApplySpeedLimit(veh, speedLimit)
             end
-            EngageRpmLimit()
-            if Config.notify then
-                lib.notify({description = Config.notifyengaged, type = 'success'})
+            -- Apply RPM limit
+            local maxRpm = 0.2 + (GetEntitySpeed(veh) / (Config.rpmlimiter * GetVehicleCurrentGear(veh)))
+            if GetVehicleCurrentRpm(veh) > maxRpm then
+                SetVehicleCurrentRpm(veh, maxRpm)
             end
         end
-    end,
-})
-
-lib.onCache('seat', function(value)
-    if value == -1 then 
-        keybind:disable(false)
-    else
-        keybind:disable(true)
-        enabled = false
+        Wait(100)
+    end
+    if not sundayDrivingEnabled then
         SetVehicleMaxSpeed(cache.vehicle, 300 / speedMultiplier)
     end
-end)
+end
+
+-- Keybind
+lib.addKeybind({
+    name = 'sundaydriving_bind',
+    description = 'Sunday Driving',
+    defaultKey = Config.keybind,
+    onPressed = function()
+        if cache.seat ~= -1 then
+            return
+        end
+        EngageDrivingLimits()
+    end
+})
